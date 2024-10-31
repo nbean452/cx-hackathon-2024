e92b5c3c3dc151b6ba7822798a08e13e341f7cde
Nicholas Benedict <nicholasb1537@gmail.com>
2024-10-31 20:39:05 +0800

    added `shadcn` components

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,11 +1,15 @@
+import { Card } from "@components/ui/card";
+
 const HomePage = () => {
   // print env
-  console.log(process.env.NEXT_PUBLIC_API_ENDPOINT);
+  const endpoint = process.env.NEXT_PUBLIC_API_ENDPOINT;
 
   return (
     <>
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <p>content goes here...</p>
+      <main className="flex flex-col gap-8 row-start-2 items-center text-center sm:items-start">
+        <Card>
+          <p>{endpoint}</p>
+        </Card>
       </main>
       <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
         <p>footer here...</p>
