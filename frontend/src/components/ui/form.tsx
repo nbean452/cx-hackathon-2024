fa3f42217f62cb810d8a0de003228957b2b005eb
Nicholas Benedict <nicholasb1537@gmail.com>
2024-10-31 21:01:31 +0800

    fix `form.tsx` eslint error

--- frontend/src/components/ui/form.tsx
+++ frontend/src/components/ui/form.tsx
@@ -80,7 +80,9 @@ FormItem.displayName = "FormItem";
 
 const FormLabel = React.forwardRef<
   React.ElementRef<typeof LabelPrimitive.Root>,
-  React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root>
+  {
+    className?: string;
+  } & React.ComponentPropsWithoutRef<typeof LabelPrimitive.Root>
 >(({ className, ...props }, ref) => {
   const { error, formItemId } = useFormField();
 
